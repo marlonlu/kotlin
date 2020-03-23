1aab19a5d028e536653ea02c61166a9b9a4e4df2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-03-23 17:17:15 +0300

    Wizard: Fix MPP mobile application template

--- idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaVelocityEngineTemplateService.kt
+++ idea/idea-new-project-wizard/src/org/jetbrains/kotlin/tools/projectWizard/wizard/service/IdeaVelocityEngineTemplateService.kt
@@ -9,12 +9,22 @@ import com.intellij.ide.fileTemplates.FileTemplateUtil
 import org.jetbrains.kotlin.tools.projectWizard.core.service.TemplateEngineService
 import org.jetbrains.kotlin.tools.projectWizard.templates.FileTemplateDescriptor
 import org.jetbrains.kotlin.tools.projectWizard.templates.Template
+import java.util.*
 
 class IdeaVelocityEngineTemplateService : TemplateEngineService, IdeaWizardService {
     override fun renderTemplate(template: FileTemplateDescriptor, data: Map<String, Any?>): String {
         val templatePath = template.templateId
-        val templateText = Template::class.java.getResource(templatePath).readText()
-        return FileTemplateUtil.mergeTemplate(data, templateText, false)
+        val templateText = try {
+             Template::class.java.getResource(templatePath).readText()
+        } catch (e: Throwable) {
+            println(e)
+            throw e
+        }
+        val properties = Properties().apply { putAll(data) }
+        return FileTemplateUtil.mergeTemplate(properties, templateText, false) { e ->
+            println(e)
+            throw e
+        }
     }
 
 }
